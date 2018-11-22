5c2b8ad6617635a073b0881946fb06bc936643a9
Adriana <s.aguirre.adriana@gmail.com>
2018-11-21 21:48:14 -0600

    Subiendo repo

---
+++ ex-01-sumPositives.js
@@ -0,0 +1,49 @@
+/**
+ * sumPositives()
+ *
+ *  Write a function called sumPositives that takes an array of numbers and
+ *  only adds the positive numbers
+ *
+*/
+
+
+// ++ YOUR CODE below
+
+
+//Escribiendo la función
+function sumPositives(numbers){
+	var suma = 0;
+//Recorre la función
+    for (var i = 0; i < numbers.length; i++){  
+      if (numbers[i] > 0){
+      	suma = suma + numbers[i];
+   	  }
+    }
+    return suma;
+}
+
+
+
+
+
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+
+console.group('ex-01');
+  console.log('%cFunction: sumPositives', 'background-color: green; color: white')
+console.groupEnd();
+
+console.log('Should return 12 in the next [3, -1, 4, 5, -3, -4] series');
+console.assert(sumPositives([3, -1, 4, 5, -3, -4]) === 12);
+
+console.log('Should return 170 in the next [-11, 30,-20, 40, 100] series');
+console.assert(sumPositives([-11, 30,-20, 40, 100]) === 170);
+
+console.log('Should return 205 in the next [4 ,-10,-30, -4, 201] series');
+console.assert(sumPositives([4 ,-10,-30, -4, 201]) === 205);
+
+console.log('\n')
